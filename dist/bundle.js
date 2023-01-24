708c824b346c206e2051e160cabf1553327bd716
Seyit Enes <seyit7enes@gmail.com>
2023-01-24 21:05:14 +0100

    done with styling

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,28 +6,27 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => {
-  // webpackBootstrap
-  /******/ var __webpack_modules__ = {
-    /***/ "./src/index.js":
-      /*!**********************!*\
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-      /***/ () => {
-        eval("\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+/***/ (() => {
 
-        /***/
-      },
+eval("\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
 
-    /******/
-  };
-  /************************************************************************/
-  /******/
-  /******/ // startup
-  /******/ // Load entry module and return exports
-  /******/ // This entry module can't be inlined because the eval devtool is used.
-  /******/ var __webpack_exports__ = {};
-  /******/ __webpack_modules__["./src/index.js"]();
-  /******/
-  /******/
-})();
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
