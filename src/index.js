ddd288786af953d3969563ae767566e2c877d918
Seyit Enes <seyit7enes@gmail.com>
2023-01-27 08:00:40 +0100

    sidebar logice

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const btnNewProject = document.getElementById("btnNewProject");
 
 function selectProject() {
   const dateProjects = document.querySelectorAll(".wrapper-project-item");
-  const mainTitle = document.querySelector(".main-title");
+  const mainTitle = document.getElementById("mainTitle");
   dateProjects.forEach((item) => {
     item.addEventListener("click", (e) => {
       dateProjects.forEach((el) => {
@@ -15,6 +15,7 @@ function selectProject() {
     });
   });
 }
+
 function addNewProject() {
   const createProject = document.createElement("div");
   createProject.classList.add("wrapper-project-item");
@@ -33,17 +34,19 @@ function addNewProject() {
   projectIcon.appendChild(path);
   projectIcon.classList.add("item-logo");
 
+  const createItemName = document.createElement("h5");
+  createItemName.textContent = "Untitled";
+  createItemName.classList.add("project-item-name");
+  createProject.appendChild(projectIcon);
+  createProject.appendChild(createItemName);
+
   const userProjectsWrapper = document.body.querySelector(
     ".wrapper-user-projects"
   );
-  const projectItemName = document.createElement("h5");
-  projectItemName.textContent = "new item";
-  projectItemName.classList.add("project-item-name");
-  createProject.appendChild(projectIcon);
-  createProject.appendChild(projectItemName);
   userProjectsWrapper.appendChild(createProject);
 }
 selectProject();
+
 btnNewProject.addEventListener("click", () => {
   addNewProject();
   selectProject();
