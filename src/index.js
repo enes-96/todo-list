2b4a1394bc20121bd3d9024498e82db82fad8ea4
Enes <seyit7enes@gmail.com>
2023-01-28 18:33:06 +0100

    working on context menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-const btnNewProject = document.getElementById("btnNewProject");
-const userProjectsWrapper = document.querySelector(".wrapper-user-projects");
+const userProjectsContainer = document.querySelector(".wrapper-user-projects");
 
-btnNewProject.addEventListener("click", () => {
+document.getElementById("btnNewProject").addEventListener("click", () => {
   addNewProject();
   selectProject();
 });
@@ -12,61 +11,78 @@ function selectProject() {
 
   allProjects.forEach((item) => {
     item.addEventListener("click", (e) => {
+      item.classList.add("selected");
+      mainTitle.textContent = item.textContent;
       allProjects.forEach((el) => {
-        item.classList.add("selected");
-        mainTitle.textContent = item.textContent;
         if (el !== item) {
           el.classList.remove("selected");
         }
       });
-      const projectItems = document.querySelectorAll(
-        ".wrapper-user-projects .wrapper-project-item"
-      );
-      projectItems.forEach((item) => {
-        item.addEventListener("dblclick", () => {
-          item.remove();
-        });
-      });
     });
   });
+  const projectItems = document.querySelectorAll(
+    ".wrapper-user-projects .wrapper-project-item"
+  );
+  projectItems.forEach((item) => addContextMenu(item));
 }
 
 function addNewProject() {
   const createProject = document.createElement("div");
   createProject.classList.add("wrapper-project-item");
-
-  const projectIcon = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttributeNS(
-    null,
-    "d",
-    "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-  );
-  projectIcon.setAttribute("viewBox", "0 0 24 24");
-  projectIcon.appendChild(path);
-  projectIcon.classList.add("item-logo");
-  createProject.appendChild(projectIcon);
+  function createIcon() {
+    const projectIcon = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const iconPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    iconPath.setAttributeNS(
+      null,
+      "d",
+      "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+    );
+    projectIcon.setAttribute("viewBox", "0 0 24 24");
+    projectIcon.appendChild(iconPath);
+    projectIcon.classList.add("item-logo");
+    createProject.appendChild(projectIcon);
+  }
+  createIcon();
 
   //----------------
 
-  const createItemName = document.createElement("input");
-  createProject.appendChild(createItemName);
-  const h5 = document.createElement("h5");
+  const createProjectNameInput = document.createElement("input");
+  createProject.appendChild(createProjectNameInput);
+  const newProjectName = document.createElement("h5");
 
-  createItemName.addEventListener("input", (e) => {
+  createProjectNameInput.addEventListener("input", (e) => {
     mainTitle.textContent = e.target.value;
   });
-  createItemName.addEventListener("focusout", (e) => {
-    h5.innerText = e.target.value;
-    h5.classList.add("project-item-name");
-    createProject.replaceChild(h5, createItemName);
+  createProjectNameInput.addEventListener("focusout", (e) => {
+    newProjectName.innerText = e.target.value;
+    newProjectName.classList.add("project-item-name");
+    createProject.replaceChild(newProjectName, createProjectNameInput);
   });
 
   //---------------------------------------------
-  userProjectsWrapper.appendChild(createProject);
+  userProjectsContainer.appendChild(createProject);
+}
+
+function addContextMenu(contextItem) {
+  const contextContainer = document.querySelector(".context-menu");
+  contextItem.addEventListener("contextmenu", handleContextMenu);
+
+  function handleContextMenu(e) {
+    contextContainer.classList.toggle("hidden");
+    contextContainer.style.left = `${e.clientX + window.scrollX}px`;
+    contextContainer.style.top = `${e.clientY + window.scrollY}px`;
+    e.preventDefault();
+  }
+
+  document.body.addEventListener("click", () => {
+    contextContainer.classList.add("hidden");
+  });
 }
 
 selectProject();
