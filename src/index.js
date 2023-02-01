db1c98d041640cb742c70f1d2bc0c18279370fbf
Seyit Enes <seyit7enes@gmail.com>
2023-02-01 21:04:52 +0100

    done with delete task

--- src/index.js
+++ src/index.js
@@ -1,133 +1,8 @@
-document.getElementById("btnNewProject").addEventListener("click", () => {
-  addNewProject();
-  selectProject();
-});
-//sidebar select
-function selectProject() {
-  const allProjects = document.querySelectorAll(".wrapper-project-item");
-  const mainTitle = document.getElementById("mainTitle");
+import sidebarJS from "./sidebar.js";
 
-  //loop over all projects and listen for click
-  allProjects.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      item.classList.add("selected");
-      mainTitle.textContent = item.textContent;
-      allProjects.forEach((el) => {
-        if (el !== item) {
-          el.classList.remove("selected");
-        }
-      });
-    });
-  });
-}
-//sidebar
-function addNewProject() {
-  const userProjectsContainer = document.querySelector(".wrp-user-projct");
+sidebarJS();
 
-  //item wrapper (icon and name) and syling
-  const createProject = document.createElement("div");
-  createProject.classList.add("wrapper-project-item", "project-item");
-
-  (function createIcon() {
-    const projectIcon = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    const iconPath = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "path"
-    );
-    iconPath.setAttributeNS(
-      null,
-      "d",
-      "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-    );
-    projectIcon.setAttribute("viewBox", "0 0 24 24");
-    projectIcon.appendChild(iconPath);
-    projectIcon.classList.add("item-logo");
-    createProject.appendChild(projectIcon);
-  })();
-
-  const createProjectNameInput = document.createElement("input");
-  createProject.appendChild(createProjectNameInput);
-  window.setTimeout(() => createProjectNameInput.focus(), 0);
-
-  createProjectNameInput.addEventListener("input", (e) => {
-    mainTitle.textContent = e.target.value;
-  });
-
-  const newProjectName = document.createElement("h5");
-  createProjectNameInput.addEventListener("keydown", handleInputToTitle);
-
-  function handleInputToTitle(e) {
-    if (e.key === "Enter") {
-      newProjectName.textContent = e.target.value;
-      newProjectName.classList.add("project-item-name");
-      createProject.replaceChild(newProjectName, createProjectNameInput);
-      //if input is empty remove item
-      if (!newProjectName.innerText) {
-        createProject.remove();
-      }
-    }
-  }
-  function editProject() {
-    const projectItems = document.querySelectorAll(".project-item");
-    const editBtn = document.querySelector(".edit-user-project");
-    //edit only the selected item
-    editBtn.addEventListener("click", () => {
-      projectItems.forEach((item) => {
-        if (item.classList.contains("selected")) {
-          editItem(item);
-        }
-      });
-    });
-    //transform h5 title to input
-    function editItem(item) {
-      item.replaceChild(createProjectNameInput, newProjectName);
-      window.setTimeout(() => createProjectNameInput.focus(), 0);
-    }
-  }
-
-  //append item to his wrapper
-  userProjectsContainer.appendChild(createProject);
-  addContextMenu(createProject);
-  deleteProject();
-  editProject();
-}
-//right click function
-function addContextMenu(contextItem) {
-  const contextContainer = document.querySelector(".context-menu");
-  contextItem.addEventListener("contextmenu", handleContextMenu);
-
-  function handleContextMenu(e) {
-    contextContainer.style.left = `${e.clientX + window.scrollX}px`;
-    contextContainer.style.top = `${e.clientY + window.scrollY}px`;
-    contextContainer.style.display = "block";
-    e.preventDefault();
-    contextContainer.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-    });
-  }
-
-  document.body.addEventListener("click", () => {
-    contextContainer.style.display = "none";
-  });
-}
-//delete from context menu
-function deleteProject() {
-  const projectItems = document.querySelectorAll(".project-item");
-  const deleteBtn = document.querySelector(".delete-user-project");
-  //if project is selected,then it can be deletet
-  deleteBtn.addEventListener("click", () => {
-    projectItems.forEach((item) => {
-      if (item.classList.contains("selected")) {
-        item.remove();
-        mainTitle.innerHtml = "";
-      }
-    });
-  });
-}
-
+//new item modal
 function manageItem() {
   //new task button
   document.body.querySelector(".add-new-task").addEventListener("click", () => {
@@ -269,17 +144,18 @@ function createNewTask(taskTitle, taskDate) {
   function createComment() {}
 
   function createEditButton() {
-    const taskIconWrapper = document.querySelector(".task-icon-wrapper");
-    taskIconWrapper.style.display = "none";
+    const taskIcon = document.querySelector(".task-icon-wrapper");
+    taskIcon.style.display = "none";
 
-    taskIconWrapper.addEventListener("click", handleTaskModal);
-
-    const taskModalSmall = document.querySelector(".task-menu-sm");
-    const taskModalOverlay = document.querySelector(".task-menu-overlay");
+    //open modal if click the dots
+    taskIcon.addEventListener("click", handleTaskModal);
 
     function handleTaskModal() {
       const newTableRowPositions = newTableRow.getBoundingClientRect();
-      //&&maybe add selected to closest
+      //modal container
+      const taskModalSmall = document.querySelector(".task-menu-sm");
+      const taskModalOverlay = document.querySelector(".task-menu-overlay");
+
       taskModalSmall.style.top = `${newTableRowPositions.top}px`;
       taskModalSmall.style.left = `${newTableRowPositions.left}px`;
       taskModalSmall.classList.remove("hidden");
@@ -293,7 +169,6 @@ function createNewTask(taskTitle, taskDate) {
     }
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    //i should make a object, when use creates todo it goes there and it is saved
     deleteAddedTask();
 
     function deleteAddedTask() {
@@ -301,9 +176,8 @@ function createNewTask(taskTitle, taskDate) {
       const removeTaskButton = document.querySelector(".delete-task");
 
       allTasks.forEach((item) => {
-        item.addEventListener("click", (e) => {
+        item.addEventListener("mouseover", (e) => {
           item.classList.add("task-selected");
-
           allTasks.forEach((el) => {
             if (el !== item) {
               el.classList.remove("task-selected");
@@ -311,21 +185,28 @@ function createNewTask(taskTitle, taskDate) {
           });
         });
       });
+
+      removeTaskButton.addEventListener("click", () => {
+        const selectedTask = document.querySelector(".task-selected");
+        if (selectedTask) {
+          selectedTask.remove();
+        }
+      });
     }
 
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+    //dots icon appear when hover
     newTableRow.addEventListener("mouseover", handleButtonPosition);
-
-    handleButtonExistins(taskIconWrapper, "mouseover", taskIconWrapper, "flex");
-    handleButtonExistins(taskIconWrapper, "mouseout", taskIconWrapper, "none");
-    handleButtonExistins(newTableRow, "mouseout", taskIconWrapper, "none");
+    handleButtonExistins(taskIcon, "mouseover", taskIcon, "flex");
+    handleButtonExistins(taskIcon, "mouseout", taskIcon, "none");
+    handleButtonExistins(newTableRow, "mouseout", taskIcon, "none");
 
     function handleButtonPosition() {
       const newTableRowPositions = newTableRow.getBoundingClientRect();
-      taskIconWrapper.style.top = `${newTableRowPositions.top}px`;
-      taskIconWrapper.style.left = newTableRowPositions.left - 40 + "px";
-      taskIconWrapper.style.display = "flex";
+      taskIcon.style.top = `${newTableRowPositions.top}px`;
+      taskIcon.style.left = newTableRowPositions.left - 40 + "px";
+      taskIcon.style.display = "flex";
     }
     function handleButtonExistins(target, event, parent, displayItem) {
       target.addEventListener(event, function () {
@@ -336,4 +217,3 @@ function createNewTask(taskTitle, taskDate) {
 }
 
 manageItem();
-selectProject();
---
+++ src/sidebar.js
@@ -0,0 +1,132 @@
+export default function sidebarJS() {
+  document.getElementById("btnNewProject").addEventListener("click", () => {
+    addNewProject();
+    selectProject();
+  });
+  //sidebar select
+  function selectProject() {
+    const allProjects = document.querySelectorAll(".wrapper-project-item");
+    const mainTitle = document.getElementById("mainTitle");
+
+    //loop over all projects and listen for click
+    allProjects.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        item.classList.add("selected");
+        mainTitle.textContent = item.textContent;
+        allProjects.forEach((el) => {
+          if (el !== item) {
+            el.classList.remove("selected");
+          }
+        });
+      });
+    });
+  }
+  //sidebar
+  function addNewProject() {
+    const userProjectsContainer = document.querySelector(".wrp-user-projct");
+
+    //item wrapper (icon and name) and syling
+    const createProject = document.createElement("div");
+    createProject.classList.add("wrapper-project-item", "project-item");
+
+    (function createIcon() {
+      const projectIcon = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+      );
+      const iconPath = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      );
+      iconPath.setAttributeNS(
+        null,
+        "d",
+        "M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+      );
+      projectIcon.setAttribute("viewBox", "0 0 24 24");
+      projectIcon.appendChild(iconPath);
+      projectIcon.classList.add("item-logo");
+      createProject.appendChild(projectIcon);
+    })();
+
+    const createProjectNameInput = document.createElement("input");
+    createProject.appendChild(createProjectNameInput);
+    window.setTimeout(() => createProjectNameInput.focus(), 0);
+
+    createProjectNameInput.addEventListener("input", (e) => {
+      mainTitle.textContent = e.target.value;
+    });
+
+    const newProjectName = document.createElement("h5");
+    createProjectNameInput.addEventListener("keydown", handleInputToTitle);
+
+    function handleInputToTitle(e) {
+      if (e.key === "Enter") {
+        newProjectName.textContent = e.target.value;
+        newProjectName.classList.add("project-item-name");
+        createProject.replaceChild(newProjectName, createProjectNameInput);
+        //if input is empty remove item
+        if (!newProjectName.innerText) {
+          createProject.remove();
+        }
+      }
+    }
+    function editProject() {
+      const projectItems = document.querySelectorAll(".project-item");
+      const editBtn = document.querySelector(".edit-user-project");
+      //edit only the selected item
+      editBtn.addEventListener("click", () => {
+        projectItems.forEach((item) => {
+          if (item.classList.contains("selected")) {
+            editItem(item);
+          }
+        });
+      });
+      //transform h5 title to input
+      function editItem(item) {
+        item.replaceChild(createProjectNameInput, newProjectName);
+        window.setTimeout(() => createProjectNameInput.focus(), 0);
+      }
+    }
+
+    //append item to his wrapper
+    userProjectsContainer.appendChild(createProject);
+    addContextMenu(createProject);
+    deleteProject();
+    editProject();
+  }
+  //right click function
+  function addContextMenu(contextItem) {
+    const contextContainer = document.querySelector(".context-menu");
+    contextItem.addEventListener("contextmenu", handleContextMenu);
+
+    function handleContextMenu(e) {
+      contextContainer.style.left = `${e.clientX + window.scrollX}px`;
+      contextContainer.style.top = `${e.clientY + window.scrollY}px`;
+      contextContainer.style.display = "block";
+      e.preventDefault();
+      contextContainer.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+      });
+    }
+
+    document.body.addEventListener("click", () => {
+      contextContainer.style.display = "none";
+    });
+  }
+  //delete from context menu
+  function deleteProject() {
+    const projectItems = document.querySelectorAll(".project-item");
+    const deleteBtn = document.querySelector(".delete-user-project");
+    //if project is selected,then it can be deletet
+    deleteBtn.addEventListener("click", () => {
+      projectItems.forEach((item) => {
+        if (item.classList.contains("selected")) {
+          item.remove();
+          mainTitle.innerHtml = "";
+        }
+      });
+    });
+  }
+  selectProject();
+}
