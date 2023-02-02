923f520840409c62771f6a57be90cf78891fc8fc
Seyit Enes <seyit7enes@gmail.com>
2023-02-02 23:52:18 +0100

    done with comment

--- src/index.js
+++ src/index.js
@@ -8,23 +8,24 @@ function manageItem() {
   document.body.querySelector(".add-new-task").addEventListener("click", () => {
     toggleModal();
   });
+
   const taskModal = document.querySelector(".modal");
 
   function submitTask() {
-    const taskSubmit = taskModal.querySelector(".task-submit");
-
-    taskSubmit.addEventListener("click", () => {
-      toggleModal();
+    //submit button
+    taskModal.querySelector(".task-submit").addEventListener("click", () => {
       const setTaskName = taskModal.querySelector(".change-title");
       const setTasDate = taskModal.querySelector(".change-date");
       const setTaskPriority = document.querySelector(".change-priority");
-      const setTaskComment = document.querySelector(".comment");
-      createNewTask(setTaskName.value, setTasDate.value);
+      const setTaskComment = taskModal.querySelector(".comment");
+      createNewTask(setTaskName.value, setTasDate.value, setTaskComment.value);
 
       //reset modal
       setTaskName.value = "";
       setTasDate.value = "";
       setTaskPriority.value = "";
+      setTaskComment.value = "";
+      toggleModal();
     });
   }
 
@@ -34,8 +35,8 @@ function manageItem() {
   }
 
   function toggleModal() {
-    taskModal.classList.toggle("hidden");
     const overlay = document.querySelector(".overlay");
+    taskModal.classList.toggle("hidden");
     overlay.classList.toggle("hidden");
   }
 
@@ -43,10 +44,14 @@ function manageItem() {
   deleteTask();
 }
 
-function createNewTask(taskTitle, taskDate) {
+function createNewTask(taskTitle, taskDate, taskComment) {
+  //select the table body
   const tableRow = document.querySelector("tbody");
+  //create new table row
   const newTableRow = document.createElement("tr");
+  //every createt task gets the clas new-task
   newTableRow.classList.add("new-task");
+  //append the tasl to the table body
   tableRow.appendChild(newTableRow);
 
   createEditButton();
@@ -63,6 +68,49 @@ function createNewTask(taskTitle, taskDate) {
   //delete task
   deleteAddedTask();
 
+  function createEditButton() {
+    // icon with 3 dots left of the task on hover
+    const taskIcon = document.querySelector(".task-icon-wrapper");
+    taskIcon.style.display = "none";
+
+    //dots icon appear when hover,and hover out when mouseout
+    newTableRow.addEventListener("mouseover", handleButtonPosition);
+    handleButtonExistins(taskIcon, "mouseover", taskIcon, "flex");
+    handleButtonExistins(taskIcon, "mouseout", taskIcon, "none");
+    handleButtonExistins(newTableRow, "mouseout", taskIcon, "none");
+
+    function handleButtonPosition() {
+      const newTableRowPositions = newTableRow.getBoundingClientRect();
+      taskIcon.style.top = `${newTableRowPositions.top}px`;
+      taskIcon.style.left = newTableRowPositions.left - 40 + "px";
+      taskIcon.style.display = "flex";
+    }
+    function handleButtonExistins(target, event, parent, displayItem) {
+      target.addEventListener(event, function () {
+        parent.style.display = displayItem;
+      });
+    }
+    //open modal if click the dots
+    taskIcon.addEventListener("click", handleTaskModal);
+
+    function handleTaskModal() {
+      //find the position of the hovered row
+      const newTableRowPositions = newTableRow.getBoundingClientRect();
+      //modal container
+      const taskModalSmall = document.querySelector(".task-menu-sm");
+      const taskModalOverlay = document.querySelector(".task-menu-overlay");
+      //set position right of the dots icon
+      taskModalSmall.style.top = `${newTableRowPositions.top}px`;
+      taskModalSmall.style.left = `${newTableRowPositions.left}px`;
+      taskModalSmall.classList.remove("hidden");
+      //click overlay to hide
+      taskModalOverlay.classList.remove("hidden");
+      taskModalOverlay.addEventListener("click", () => {
+        taskModalSmall.classList.add("hidden");
+        taskModalOverlay.classList.add("hidden");
+      });
+    }
+  }
   function createTaskProperty(
     elementProperty,
     elementClass,
@@ -95,11 +143,15 @@ function createNewTask(taskTitle, taskDate) {
   }
   function createTitle() {
     const newTaskTitle = createTaskProperty("td", "title-wrapper", newTableRow);
-
+    const newTaskTitleWrapper = createTaskProperty(
+      "div",
+      "title-icon-wrapper",
+      newTaskTitle
+    );
     const newTitleInput = createTaskProperty(
       "input",
       "todo-title",
-      newTaskTitle,
+      newTaskTitleWrapper,
       taskTitle
     );
     if (!newTitleInput.value) {
@@ -107,7 +159,6 @@ function createNewTask(taskTitle, taskDate) {
     }
     if (newTitleInput.value.trim().length === 0) {
       newTitleInput.placeholder = "untitled";
-
       return (newTitleInput.value = "");
     }
   }
@@ -143,34 +194,32 @@ function createNewTask(taskTitle, taskDate) {
     newTaskPriority.appendChild(priorityWrapper);
     newTableRow.appendChild(newTaskPriority);
   }
+
   function createComment() {
-    const addCommentButton = document.querySelector(".comment-task");
-
-    addCommentButton.addEventListener("click", () => {
-      const selectedTask = document.querySelector(".task-selected");
-      const taskModalSmall = document.querySelector(".task-menu-sm");
-      const commentContainer = document.querySelector(".comment-box");
-      const commentOverlay = document.querySelector(".comment-box-overlay");
-
-      if (selectedTask) {
-        commentContainer.classList.remove("hidden");
-        commentOverlay.classList.remove("hidden");
-        taskModalSmall.classList.add("hidden");
-      }
-
-      commentOverlay.addEventListener("click", () => {
-        commentContainer.classList.add("hidden");
-        commentOverlay.classList.add("hidden");
-        const taskTitle = document.querySelector(".title-wrapper");
-        //
-
-        taskTitle.style.backgroundColor = "blue";
-      });
+    const newTaskComment = createTaskProperty(
+      "td",
+      "comment-wrapper",
+      newTableRow
+    );
+    const newComment = createTaskProperty(
+      "input",
+      "user-added-comment",
+      newTaskComment,
+      taskComment
+    );
+    if (newComment.value.trim().length === 0) {
+      newComment.value = "";
+    }
+    newTaskComment.addEventListener("mouseover", () => {
+      newComment.style.display = "block";
+    });
+    newTaskComment.addEventListener("mouseleave", () => {
+      newComment.style.display = "none";
     });
   }
+
   function deleteAddedTask() {
     const allTasks = document.querySelectorAll(".new-task");
-    const removeTaskButton = document.querySelector(".delete-task");
     allTasks.forEach((item) => {
       item.addEventListener("mouseover", (e) => {
         item.classList.add("task-selected");
@@ -181,7 +230,7 @@ function createNewTask(taskTitle, taskDate) {
         });
       });
     });
-
+    const removeTaskButton = document.querySelector(".delete-task");
     removeTaskButton.addEventListener("click", () => {
       const selectedTask = document.querySelector(".task-selected");
       if (selectedTask) {
@@ -190,49 +239,6 @@ function createNewTask(taskTitle, taskDate) {
         selectedTask.remove();
       }
     });
-  }
-  function createEditButton() {
-    const taskIcon = document.querySelector(".task-icon-wrapper");
-    taskIcon.style.display = "none";
-
-    //open modal if click the dots
-    taskIcon.addEventListener("click", handleTaskModal);
-
-    function handleTaskModal() {
-      const newTableRowPositions = newTableRow.getBoundingClientRect();
-      //modal container
-      const taskModalSmall = document.querySelector(".task-menu-sm");
-      const taskModalOverlay = document.querySelector(".task-menu-overlay");
-
-      taskModalSmall.style.top = `${newTableRowPositions.top}px`;
-      taskModalSmall.style.left = `${newTableRowPositions.left}px`;
-      taskModalSmall.classList.remove("hidden");
-      if (!taskModalSmall.classList.contains("hidden")) {
-        taskModalOverlay.classList.remove("hidden");
-        taskModalOverlay.addEventListener("click", () => {
-          taskModalSmall.classList.add("hidden");
-          taskModalOverlay.classList.add("hidden");
-        });
-      }
-    }
-
-    //dots icon appear when hover
-    newTableRow.addEventListener("mouseover", handleButtonPosition);
-    handleButtonExistins(taskIcon, "mouseover", taskIcon, "flex");
-    handleButtonExistins(taskIcon, "mouseout", taskIcon, "none");
-    handleButtonExistins(newTableRow, "mouseout", taskIcon, "none");
-
-    function handleButtonPosition() {
-      const newTableRowPositions = newTableRow.getBoundingClientRect();
-      taskIcon.style.top = `${newTableRowPositions.top}px`;
-      taskIcon.style.left = newTableRowPositions.left - 40 + "px";
-      taskIcon.style.display = "flex";
-    }
-    function handleButtonExistins(target, event, parent, displayItem) {
-      target.addEventListener(event, function () {
-        parent.style.display = displayItem;
-      });
-    }
   }
 }
 
