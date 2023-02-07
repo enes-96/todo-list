d03e51098495f950a31dedeaa824f1720f411f39
Seyit Enes <seyit7enes@gmail.com>
2023-02-07 22:54:09 +0100

    filter todos

--- src/index.js
+++ src/index.js
@@ -8,37 +8,33 @@ manageTodos();
 function manageTodos() {
   const todos = [];
   const sidebar = document.querySelector(".sidebar");
+  const submitTask = document.querySelector(".task-submit");
+  const table = document.querySelector("tbody");
 
-  //push todo to todos array
   function addTodo(project, todo) {
-    todos.push({
-      project: project,
-      todo: todo,
-    });
+    todos.push({ project, todo });
   }
-  //filter todos by the project name
   function getTodos(project) {
     return todos.filter((t) => t.project === project);
   }
-  //--------------------------------------------------------------
 
   //all tasks are the todo array
-  const submitTask = document.querySelector(".task-submit");
   submitTask.addEventListener("click", () => {
     const selectedProject = sidebar.querySelector(".selected");
     const addedTask = document.querySelectorAll(".new-task");
     addedTask.forEach((task) => {
-      addTodo(selectedProject.innerText, task);
+      if (!task.classList.contains("added")) {
+        addTodo(selectedProject.innerText, task);
+        task.classList.add("added");
+      }
     });
     console.log(todos);
   });
 
-  //-----------------------------------------------------------------------
-  //filter todos by category
   const allProjects = document.querySelectorAll(".wrapper-project-item");
   allProjects.forEach((project) => {
     project.addEventListener("click", () => {
-      getTodos(project);
+      console.log(getTodos(project.innerText));
     });
   });
 }
