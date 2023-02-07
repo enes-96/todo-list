2d51a5add2e509c8a57704353b24272a352fbe80
Enes <seyit7enes@gmail.com>
2023-02-07 16:04:49 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ manageTodos();
 
 function manageTodos() {
   const todos = [];
-  const sidebar = document.querySelector(".sidebar");
-  const submitButton = document.querySelector(".task-submit");
 
   //push todo to todos array
   function addTodo(project, todo) {
@@ -21,20 +19,34 @@ function manageTodos() {
   function getTodos(project) {
     return todos.filter((t) => t.project === project);
   }
-  //
+  const submitButton = document.querySelector(".task-submit");
   submitButton.addEventListener("click", () => {
-    //i need to select all the new taks this selects always the first
-    const newTask = document.querySelector(".new-task");
-    const taskValue = newTask.querySelector(".todo-title");
+    const newTask = document.querySelectorAll(".new-task");
+    const sidebar = document.querySelector(".sidebar");
     const selectedProject = sidebar.querySelector(".selected");
 
-    addTodo(selectedProject.innerText, taskValue.value);
+    newTask.forEach((task) => {
+      addTodo(selectedProject.innerText, task);
+    });
     console.log(todos);
   });
 
+  const removeTaskButton = document.querySelector(".delete-task");
+  removeTaskButton.addEventListener("click", () => {
+    const selectedTask = document.querySelector(".task-selected");
+    if (selectedTask) {
+      const taskModalSmall = document.querySelector(".task-menu-sm");
+      taskModalSmall.classList.add("hidden");
+      selectedTask.remove();
+    }
+  });
+
+  //filter todos by category
   const allProjects = document.querySelectorAll(".wrapper-project-item");
   allProjects.forEach((project) => {
-    project.addEventListener("click", () => {});
+    project.addEventListener("click", () => {
+      getTodos(project);
+    });
   });
 }
 //all todos are in to-do
